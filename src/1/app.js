45160740097b1b538bbecbc8e48e0df3f92d2ea4
Jordan Moss <jmoss636@gmail.com>
2020-12-01 14:16:52 +0000

    day 1 part 2

--- src/1/app.js
+++ src/1/app.js
@@ -1,7 +1,7 @@
 const raw = require('fs').readFileSync('./input.txt', 'utf-8')
 const input = raw.split('\n').map(Number)
 
-function find(list) {
+function findTwo(list) {
     const TOTAL = 2020
 
     for(let i = 0; i < list.length; i++) {
@@ -11,6 +11,22 @@ function find(list) {
     }
 }
 
-const pair = find(input)
+function findThree(list) {
+    const TOTAL = 2020
+
+    for(let i = 0; i < list.length; i++) {
+        for(let j = i+1; j < list.length; j++) {
+            for(let k = j+1; k < list.length; k++) {
+                if((list[i] + list[j] + list[k]) == TOTAL) return [list[i], list[j], list[k]]
+            }
+        }
+    }
+}
+
+const pair = findTwo(input)
 console.log(`${pair[0]} + ${pair[1]} = 2020`)
 console.log(`Part One: Result is ${pair[0] * pair[1]}`)
+
+const three = findThree(input)
+console.log(`${three[0]} + ${three[1]} + ${three[2]} = 2020`)
+console.log(`Part Two: Result is ${three[0] * three[1] * three[2]}`)
