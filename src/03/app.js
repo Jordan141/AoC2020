3209a180f46d0662aa05a2d9c50c5f1f63a5095c
Jordan Moss <jmoss636@gmail.com>
2020-12-03 13:08:28 +0000

    day 3 part 1

--- src/03/app.js
+++ src/03/app.js
@@ -1,1 +1,29 @@
 const raw = require('fs').readFileSync(__dirname + '/input.txt', 'utf-8')
+const input = raw.split('\n')
+
+const TREE = '#'
+
+function makeMap(map) {
+    return {
+        __map: map,
+        getPos: (x, y) => map[y][x % map[0].length],
+        getHeight: () => map.length
+    }
+}
+
+function travelSlope(dx, dy, map) {
+    let x = 0, y = 0, trees = 0
+    
+    while(y < map.getHeight()) {
+        const currentPos = map.getPos(x, y)
+        if(currentPos === TREE) trees++
+
+        x += dx
+        y += dy
+    }
+
+    return trees
+}
+
+const gridMap = makeMap(input.map(line => [...line]))
+console.log(travelSlope(3, 1, gridMap))
