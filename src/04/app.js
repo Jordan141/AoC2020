214fc00e11531a482052c657ef2affa356e65198
Jordan Moss <jmoss636@gmail.com>
2020-12-04 12:26:11 +0000

    day 4 part 1

--- src/04/app.js
+++ src/04/app.js
@@ -1,2 +1,12 @@
 const raw = require('fs').readFileSync(__dirname + '/input.txt', 'utf-8')
-console.log(raw)
+const input = raw.split('\n\n')
+
+const VALID_FIELDS = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+const OPTIONAL_FIELDS = ['cid']
+
+function validItem(item) {
+    return VALID_FIELDS.every(i => item.match(new RegExp(i)))
+}
+
+const validPassportCount = input.filter(validItem).length
+console.log(validPassportCount)
