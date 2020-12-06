3883fbd3b6857dc0fcbcb72fed3d8f9e4c39c797
Jordan Moss <jmoss636@gmail.com>
2020-12-06 14:46:35 +0000

    day 6 part 1

--- src/06/app.js
+++ src/06/app.js
@@ -1,1 +1,11 @@
 const raw = require('fs').readFileSync(__dirname + '/input.txt', 'utf-8')
+const groups = raw.split('\n\n').map(group => group.split('\n'))
+
+function positiveAnswers(group) {
+    const answers = new Set(group.join(''))
+    return answers.size
+}
+
+//Part one
+const sum = groups.reduce((total, group) => total += positiveAnswers(group), 0)
+console.log('Sum:', sum)
