16318aed094dc861fe22225a82e99ac377b59189
Jordan Moss <jmoss636@gmail.com>
2020-12-06 15:34:57 +0000

    day 6 part 2

--- src/06/app.js
+++ src/06/app.js
@@ -1,11 +1,28 @@
 const raw = require('fs').readFileSync(__dirname + '/input.txt', 'utf-8')
 const groups = raw.split('\n\n').map(group => group.split('\n'))
 
-function positiveAnswers(group) {
-    const answers = new Set(group.join(''))
-    return answers.size
+function anyYesCount(group) {
+    const uniqueAnswers = new Set(group.join(''))
+    return uniqueAnswers.size
 }
 
 //Part one
-const sum = groups.reduce((total, group) => total += positiveAnswers(group), 0)
-console.log('Sum:', sum)
+const sum = groups.reduce((total, group) => total += anyYesCount(group), 0)
+console.log('Part One Sum:', sum)
+
+function findCommonAnswers(group) {
+    const commonAnswers = group.shift().reduce((total, curr) => {
+        if(!total.includes(curr) && group.every(person => person.includes(curr))) {
+            total.push(curr)
+        }
+
+        return total
+    }, [])
+
+    return commonAnswers
+}
+
+const groupData = groups.map(group => group.map(person => person.split('')))
+
+const partTwoSum = groupData.reduce((sum, curr) => sum += findCommonAnswers(curr).length, 0)
+console.log('Part Two Sum:', partTwoSum)
