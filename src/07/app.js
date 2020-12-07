e75750a41ba523f7f33678135124feb75198ede9
Jordan Moss <jmoss636@gmail.com>
2020-12-07 12:46:15 +0000

    day 7 part 2

--- src/07/app.js
+++ src/07/app.js
@@ -1,24 +1,59 @@
 const raw = require('fs').readFileSync(__dirname + '/input.txt', 'utf-8')
-const rules = raw.split('\n').reduce((rules, line) => {
-    const [, colour, otherColours] = /(\w+ \w+) bags contain (.*)\./.exec(line)
 
-    const compatibleWith = otherColours !== 'no other bags'
-    ? otherColours.split(', ').map((other) => /(\w+ \w+) bags?/.exec(other)[1])
-    : []
+function getRules(input, callback, reduceStarter) {
+    return input.split('\n').reduce((rules, line) => {
+        const [, colour, otherColours] = /(\w+ \w+) bags contain (.*)\./.exec(line)
+        return callback(rules, colour, otherColours)
+    }, reduceStarter)
+}
 
-    rules[colour] = new Set()
-    compatibleWith.forEach(otherColour => rules[colour].add(otherColour))
-    return rules
-}, {})
+const partOneRules = getRules(raw, partOne, [])
 
 function expand(bag) {
-    const colours = [...rules[bag].values()]
-    for(const colour of rules[bag].values()) {
+    const colours = [...partOneRules[bag].values()]
+    for(const colour of partOneRules[bag].values()) {
         colours.push(...expand(colour))
     }   
     return colours
 }
 
+function partOne(rules, colour, otherColours) {
+    const compatibleWith = otherColours !== 'no other bags'
+        ? otherColours.split(', ').map((other) => /(\w+ \w+) bags?/.exec(other)[1])
+        : []
+    
+    rules[colour] = new Set()
+    compatibleWith.forEach(otherColour => rules[colour].add(otherColour))
+    return rules
+}
+
 //Part One
-const answer = Object.keys(rules).filter(key => expand(key).includes('shiny gold')).length
-console.log(answer)
+const answer = Object.keys(partOneRules).filter(key => expand(key).includes('shiny gold')).length
+console.log('Part One:', answer)
+
+function partTwo(rules, colour, otherColour) {
+    const compatibleWith = otherColour !== 'no other bags'
+      ? otherColour.split(', ').map((other) => {
+        const [, units, colour] = /(\d+) (\w+ \w+) bags?/.exec(other)
+        return { units: parseInt(units), colour }
+      })
+      : []
+
+    rules.set(colour, [])
+    compatibleWith.forEach((otherColour) => rules.get(colour).push(otherColour))
+    return rules
+}
+
+const partTwoRules = getRules(raw, partTwo, new Map())
+
+const traverse = (bag) => {
+    let total = 0
+    for (const {colour, units} of partTwoRules.get(bag)) {
+        total += units + units * traverse(colour)
+    }
+    return total
+}
+
+//Part Two
+const otherAnswer = traverse('shiny gold')
+console.log('Part Two:', otherAnswer)
