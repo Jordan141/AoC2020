d4de14e7b42a7297f59c2bb874ca301b5ef4528a
Jordan Moss <jmoss636@gmail.com>
2020-12-13 07:22:09 +0000

    day 13 part 1

--- src/13/app.js
+++ src/13/app.js
@@ -1,1 +1,32 @@
 const raw = require('fs').readFileSync(__dirname + '/input.txt', 'utf-8')
+const EARLIEST_TIMESTAMP = Number(raw.split('\n')[0])
+const BUS_ROUTES = raw.split('\n')[1].split(',').filter(x => x !== 'x').map(Number)
+
+function timeLeft(id, time) {
+    let timeLeft = time % id
+    if(timeLeft > 0) {
+        timeLeft = id - timeLeft
+    }
+    return timeLeft
+}
+
+function partOne(input) {
+    let startTime = input[0]
+    let busIds = input[1]
+
+    let busTimeLeft = busIds.map(busId => timeLeft(busId, startTime))
+    let minWaitTime = busTimeLeft[0]
+    let minWaitTimeId = busIds[0]
+    for(let i = 1; i < busIds.length; i++) {
+        if(busTimeLeft[i] < minWaitTime) {
+            minWaitTime = busTimeLeft[i]
+            minWaitTimeId = busIds[i]
+        }
+    }
+    return minWaitTime * minWaitTimeId
+}
+
+const partOneResult = partOne([EARLIEST_TIMESTAMP, BUS_ROUTES])
+
+
+console.log(partOneResult)
